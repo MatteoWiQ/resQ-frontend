4ea405ce8b7b4fe28f6261d449f0896a28f1c3e8
Fabian Torrico <fabian.torricolazarte@gmail.com>
2026-09-10 12:52:08 -0400

    Implementar registro de usuarios conectado al backend

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { Register } from './pages/register/register';
 
-export const authRoutes: Routes = [{ path: 'login', component: LoginComponent }];
+export const authRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: Register }
+];
--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
 
+  usuario = {
+    nombre: '',
+    email: '',
+    password: '',
+    telefono: '',
+    rol: ''
+  };
+
+  constructor(private authService: AuthService) {}
+
+  onRegister() {
+    this.authService.register(this.usuario).subscribe({
+      next: (response) => {
+        console.log('Usuario registrado correctamente:', response);
+        alert('Usuario registrado correctamente');
+      },
+      error: (error) => {
+        console.error('Error al registrar usuario:', error);
+        alert('Error al registrar usuario');
+      }
+    });
+  }
 }
