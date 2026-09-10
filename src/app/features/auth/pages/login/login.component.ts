f23628444b7652da7e59e7c8292f1cc08dfa914a
Mateo Wilson <mat21quisp@gmail.conm>
2026-09-10 17:28:50 -0400

    feat(auth): agregar acceso a registro desde el login y estilizar formulario de registro

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,13 +1,14 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 import { AuthService } from '../../../../core/services/auth.service';
 import { LoginRequest } from '../../../../shared/models/auth.models';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
